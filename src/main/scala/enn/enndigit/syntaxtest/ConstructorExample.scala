92c4e49fd946b6acf0ffe73f9c65d70e3b915436
chenchen <altenchen@aliyun.com>
2018-09-07 15:10:54 +0800

    改 PageView主构造参数的申明类型

---
+++ src/main/scala/enn/enndigit/syntaxtest/ConstructorExample.scala
@@ -0,0 +1,12 @@
+package enn.enndigit.syntaxtest
+
+/**
+  * @Author:chenchen
+  * @Description:
+  * @Date:2018 /9/7
+  * @Project:sparktest
+  * @Package:enn.enndigit.syntaxtest
+  */
+class ConstructorExample {
+
+}
